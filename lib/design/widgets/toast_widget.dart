1c44389fe9093907474d9cb2126141ea8b4382c9
Sultonbek <apple@MacBook-Air.local>
2024-05-09 18:10:55 +0500

    feat: add bottom_nav_bar

---
+++ lib/design/widgets/toast_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showToast(String message, {ToastGravity gravity = ToastGravity.TOP}) {
+  return FToast().showToast(
+    gravity: gravity,
+    toastDuration: const Duration(seconds: 3),
+    child: GestureDetector(
+      onTap: () => FToast().removeQueuedCustomToasts(),
+      child: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
+                  message,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
