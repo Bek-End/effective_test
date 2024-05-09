b110d7fd47b9fca52d865000b649f2d052b7a71f
Sultonbek <apple@MacBook-Air.local>
2024-05-09 18:58:31 +0500

    feat: add const/app_colors

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const black = Color(0xFF0C0C0C);
+  static const grey1 = Color(0xFF1D1E20);
+  static const grey2 = Color(0xFF2F3035);
+  static const grey3 = Color(0xFF2F3035);
+  static const grey4 = Color(0xFF3E3F43);
+  static const grey5 = Color(0xFF5E5F61);
+  static const grey6 = Color(0xFF9F9F9F);
+  static const grey7 = Color(0xFFDBDBDB);
+  static const white = Color(0xFFFFFFFF);
+  static const blue = Color(0xFF2261BC);
+  static const darkBlue = Color(0xFF00427D);
+  static const green = Color(0xFF3A633B);
+  static const darkGreen = Color(0xFF1E341F);
+  static const red = Color(0xFFFF5E5E);
+  static const orange = Color(0xFFF36E36);
+}
