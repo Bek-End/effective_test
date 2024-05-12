db6df66bf77d18359410ac5e21fdc1cab4e29e08
Sultonbek <apple@MacBook-Air.local>
2024-05-12 13:30:05 +0500

    feat: tickets screen ui

--- lib/design/screens/search_result_screen.dart
+++ lib/design/screens/search_result_screen.dart
@@ -2,15 +2,14 @@ import 'dart:math';
 
 import 'package:effective_test/common/constants/app_colors.dart';
 import 'package:effective_test/common/constants/assets.dart';
+import 'package:effective_test/design/screens/tickets_screen.dart';
 import 'package:effective_test/design/widgets/app_theme.dart';
 import 'package:effective_test/design/widgets/bottom_sheet_with_header.dart';
 import 'package:effective_test/design/widgets/filled_button_widget.dart';
 import 'package:effective_test/design/widgets/rounded_button_widget.dart';
 import 'package:effective_test/design/widgets/target_point_widget.dart';
 import 'package:effective_test/design/widgets/text_field_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 
 class SearchResultScreen extends StatefulWidget {
@@ -109,9 +108,7 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
                         const SizedBox(width: 8),
                         Text(
                           'обратно',
-                          style: themeText.title4.copyWith(
-                            fontStyle: FontStyle.italic,
-                          ),
+                          style: themeText.title4,
                         )
                       ],
                     ),
@@ -122,15 +119,12 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
                       child: RichText(
                         text: TextSpan(
                           text: '24 фев',
-                          style: themeText.title4.copyWith(
-                            fontStyle: FontStyle.italic,
-                          ),
+                          style: themeText.title4,
                           children: [
                             TextSpan(
                               text: ', сб',
                               style: themeText.title4.copyWith(
                                 color: colorScheme.basic.shade6,
-                                fontStyle: FontStyle.italic,
                               ),
                             ),
                           ],
@@ -147,9 +141,7 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
                         const SizedBox(width: 8),
                         Text(
                           '1,эконом',
-                          style: themeText.title4.copyWith(
-                            fontStyle: FontStyle.italic,
-                          ),
+                          style: themeText.title4,
                         )
                       ],
                     ),
@@ -163,9 +155,7 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
                         const SizedBox(width: 8),
                         Text(
                           'фильтры',
-                          style: themeText.title4.copyWith(
-                            fontStyle: FontStyle.italic,
-                          ),
+                          style: themeText.title4,
                         )
                       ],
                     ),
@@ -214,16 +204,12 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
                                         Expanded(
                                           child: Text(
                                             'Уральские авиалинии',
-                                            style: themeText.title4.copyWith(
-                                              fontStyle: FontStyle.italic,
-                                            ),
+                                            style: themeText.title4,
                                           ),
                                         ),
                                         Text(
                                           '2 390 ₽ ',
-                                          style: themeText.title4.copyWith(
-                                            fontStyle: FontStyle.italic,
-                                          ),
+                                          style: themeText.title4,
                                         ),
                                         SvgPicture.asset(
                                             Assets.icons.arrowRightIos),
@@ -232,9 +218,7 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
                                     Text(
                                       '07:00  09:10   10:00  11:00  12:00  13:00  12:00      ',
                                       overflow: TextOverflow.ellipsis,
-                                      style: themeText.title4.copyWith(
-                                        fontStyle: FontStyle.italic,
-                                      ),
+                                      style: themeText.title4,
                                     )
                                   ],
                                 ),
@@ -262,7 +246,9 @@ class _SearchResultScreenState extends State<SearchResultScreen> {
             ),
             const SizedBox(height: 23),
             FilledButtonWidget(
-              onTap: () {},
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => const TicketsScreen(),
+              )),
               child: Text(
                 'Посмотреть все билеты',
                 style: themeText.buttonText.copyWith(
